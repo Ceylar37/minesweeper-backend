e1b4458603cb09faf6a49a6fdfd1094b8b0ffaf3
Ceylar37 <69920677+Ceylar37@users.noreply.github.com>
2022-06-15 22:19:57 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
-  app.enableCors({credentials: true, origin: process.env.CLIENT_ORIGIN});
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
