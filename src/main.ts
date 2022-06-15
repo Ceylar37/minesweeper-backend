411879f7a7d698b68ec76e2565a7671d1f931613
Ceylar37 <69920677+Ceylar37@users.noreply.github.com>
2022-06-15 20:47:34 +0300

    client origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
-  app.enableCors({credentials: true, origin: "http://localhost:3000"});
+  app.enableCors({credentials: true, origin: "https://minesweeper-frontend-zeta.vercel.app/"});
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
